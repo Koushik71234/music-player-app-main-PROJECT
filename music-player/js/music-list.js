1027be894d0d9d6a35f90fb2c373305ee6b6258c
koushik <koushikmanikanta7@gmail.com>
2024-08-15 18:02:07 +0530

    first commit

---
+++ music-player/js/music-list.js
@@ -0,0 +1,138 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   },
+
+let allMusic = [
+  {
+    name: "chilipiga",
+    artist: "Michael Dutchi Libranda",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "sun raha hai na",
+    artist: "Arthur Nery",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "kaatuka kanule",
+    artist: "G.v.prakasam kumar ",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "waka waka",
+    artist: "anurag kulkarni",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "aradhya",
+    artist: "sid sriram",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "ey bidda idi naa adda",
+    artist: "nakash aziz",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "saami saami",
+    artist: "mounika yadav",
+    img: "music-7",
+    src: "music-7"
+  },
+
+  {
+    name: "valmiki",
+    artist: "anurag kulkarni",
+    img: "music-8",
+    src: "music-8"
+  },
+
+  {
+    name: "nenu nuvvantu",
+    artist: "anurag kulkarni",
+    img: "music-9",
+    src: "music-9"
+  },
+
+  {
+    name: "kushi",
+    artist: "sid sriram",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "master",
+    artist: "anirudh",
+    img: "music-11",
+    src: "music-11"
+  },
+
+  {
+    name: "rooba rooba",
+    artist: "k.anirudh",
+    img: "music-12",
+    src: "music-12"
+  },
+
+  {
+    name: "dhoom dham dostan",
+    artist: "anurag kulkarni",
+    img: "music-13",
+    src: "music-13"
+  },
+
+  {
+    name: "choosi chudangane",
+    artist: "chalo",
+    img: "music-14",
+    src: "music-14"
+  },
+
+  {
+    name: "naatu naatu",
+    artist: "M.M.keervani",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "kabir singh",
+    artist: "Moira Dela Torre",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "daku",
+    artist: "chani nattan",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "saami saami",
+    artist: "maheswari",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Paubaya",
+    artist: "Moira Dela Torre",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Sana'y Di Na Lang",
+    artist: "Bandang Lapis",
+    img: "music-20",
+    src: "music-20"
+  },
+];
